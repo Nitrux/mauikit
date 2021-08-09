7a827f002916afba30451581d170d6fa4874a94c
Uri Herrera <hello@uriherrera.com>
2021-08-09 18:49:29 -0500

    create pkg

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# mauikit
-Mirror of https://invent.kde.org/maui/mauikit
+# MauiKit [![Build Status](https://travis-ci.org/Nitrux/mauikit.svg?branch=main)](https://travis-ci.org/Nitrux/mauikit)
+
+MauiKit from Git master.
+
+# Issues
+If you find problems with the contents of this repository please create an issue.
+
+©2021 Nitrux Latinoamericana S.C.
---
+++ travis-build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -x
+
+apt -qq update
+apt -qq -yy install equivs curl git wget gnupg2
+
+### FIXME - the container mauikit/ubuntu-18.04-amd64 does have the neon repo but for some idiotic reason it isn't working here
+
+wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
+	55751E5D > /dev/null
+
+apt -qq update
+
+### Install Dependencies
+
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules appstream
+
+mk-build-deps -i -t "apt-get --yes" -r
+
+### Clone repo.
+
+git clone https://invent.kde.org/maui/mauikit.git
+
+mv mauikit/* .
+
+rm -rf mauikit demo LICENSE README.md
+
+### Build Deb
+
+mkdir source
+mv ./* source/ # Hack for debuild
+cd source
+debuild -b -uc -us
