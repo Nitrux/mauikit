54854808aa8343d62e13cdc52888b488c01fe129
Uri Herrera <hello@uriherrera.com>
2021-11-09 13:38:04 -0600

    build

--- travis-build.sh
+++ travis-build.sh
@@ -22,7 +22,7 @@ mk-build-deps -i -t "apt-get --yes" -r
 
 ### Clone repo.
 
-git clone --depth 1 --branch v2.0.1 https://invent.kde.org/maui/mauikit.git
+git clone --depth 1 --branch v2.1 https://invent.kde.org/maui/mauikit.git
 
 mv mauikit/* .
 
