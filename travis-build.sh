6efadb2e8bc84b1e78bd9a242e2f09b0773bf80b
Uri Herrera <hello@uriherrera.com>
2021-11-11 17:39:52 -0600

    screw debuild use checkinstall no.2

--- travis-build.sh
+++ travis-build.sh
@@ -29,7 +29,7 @@ mv mauikit/* .
 rm -rf mauikit demo LICENSE README.md
 
 ### Build Deb
-### DO NOT USE debuild, screw it
+### DO NOT USE debuild,screw it
 
 mkdir -p mauikit/build && cd mauikit/build
 
@@ -49,14 +49,20 @@ cmake \
 make
 
 checkinstall -D -y \
-  --install=no \
-  --fstrans=yes \
-  --reset-uids=yes \
-  --pkgname=mauikit \
-  --pkgversion=2.1.0 \
-  --pkgrelease="1" \
-  --arch=amd64 \
-  --pkglicense=LGPL-3 \
-  --maintainer="Uri Herrera <uri_herrera@nxos.org>" \
-  --pakdir=../.. \
-  --requires=cmake,debhelper,extra-cmake-modules,libkf5i18n-dev,libkf5kio-dev,libkf5notifications-dev,libkf5solid-dev,libkf5syntaxhighlighting-dev,libqt5svg5-dev,libqt5x11extras5-dev,libxcb-icccm4-dev,libxcb-shape0-dev,qml-module-org-kde-kirigami2,qml-module-qtquick-controls2,qml-module-qtquick-shapes,qtbase5-dev,qtdeclarative5-dev,qtquickcontrols2-5-dev
+	--install=no \
+	--fstrans=yes \
+	--pkgname=mauikit \
+	--pkgversion=2.1.0 \
+	--pkgarch=amd64 \
+	--pkgrelease="1" \
+	--pkglicense=LGPL-3 \
+	--pkggroup=lib
+	--pkgsource=mauikit
+	--pakdir=../.. \
+	--maintainer="Uri Herrera <uri_herrera@nxos.org>" \
+	--provides=libmauikit,mauikit-dev,qml-module-org-kde-mauikit \
+	--requires=libc6,libkf5configcore5,libkf5coreaddons5,libkf5i18n5,libkf5notifications5,libqt5core5a,libqt5gui5,libqt5qml5,libstdc++6,qml-module-org-kde-kirigami2,qml-module-qtquick-controls2,qml-module-qtquick-shapes \
+	--nodoc \
+	--strip=no \
+	--stripso=yes \
+	--reset-uids=yes
