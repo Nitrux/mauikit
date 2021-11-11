e2d13689a22f864942f884d81b9a5e0449837daa
Uri Herrera <hello@uriherrera.com>
2021-11-11 17:26:23 -0600

    screw debuild use checkinstall

--- travis-build.sh
+++ travis-build.sh
@@ -3,7 +3,7 @@
 set -x
 
 apt -qq update
-apt -qq -yy install equivs curl git wget gnupg2
+apt -qq -yy install equivs curl git wget gnupg2 checkinstall
 
 ### FIXME - the container mauikit/ubuntu-18.04-amd64 does have the neon repo but for some idiotic reason it isn't working here
 
@@ -29,8 +29,34 @@ mv mauikit/* .
 rm -rf mauikit demo LICENSE README.md
 
 ### Build Deb
+### DO NOT USE debuild, screw it
 
-mkdir source
-mv ./* source/ # Hack for debuild
-cd source
-debuild -b -uc -us
+mkdir -p mauikit/build && cd mauikit/build
+
+cmake \
+	-DCMAKE_INSTALL_PREFIX=/usr \
+	-DENABLE_BSYMBOLICFUNCTIONS=OFF \
+	-DQUICK_COMPILER=ON \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_SYSCONFDIR=/etc \
+	-DCMAKE_INSTALL_LOCALSTATEDIR=/var \
+	-DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON \
+	-DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON \
+	-DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" \
+	-DCMAKE_VERBOSE_MAKEFILE=ON \
+	-DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu ..
+
+make
+
+checkinstall -D -y \
+  --install=no \
+  --fstrans=yes \
+  --reset-uids=yes \
+  --pkgname=mauikit \
+  --pkgversion=2.1.0 \
+  --pkgrelease="1" \
+  --arch=amd64 \
+  --pkglicense=LGPL-3 \
+  --maintainer="Uri Herrera <uri_herrera@nxos.org>" \
+  --pakdir=../.. \
+  --requires=cmake,debhelper,extra-cmake-modules,libkf5i18n-dev,libkf5kio-dev,libkf5notifications-dev,libkf5solid-dev,libkf5syntaxhighlighting-dev,libqt5svg5-dev,libqt5x11extras5-dev,libxcb-icccm4-dev,libxcb-shape0-dev,qml-module-org-kde-kirigami2,qml-module-qtquick-controls2,qml-module-qtquick-shapes,qtbase5-dev,qtdeclarative5-dev,qtquickcontrols2-5-dev
