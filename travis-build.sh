22d75ec902f3d50579ed68934858263157a24daa
Uri Herrera <hello@uriherrera.com>
2022-04-28 00:58:28 -0500

    update pkg

--- travis-build.sh
+++ travis-build.sh
@@ -52,7 +52,7 @@ DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
 
 ### Clone Repository
 
-git clone --depth 1 --branch v2.1.1 https://invent.kde.org/maui/mauikit.git
+git clone --depth 1 --branch master https://invent.kde.org/maui/mauikit.git
 
 rm -rf mauikit/{demo,LICENSE,README.md}
 
@@ -97,11 +97,11 @@ checkinstall -D -y \
 	--install=no \
 	--fstrans=yes \
 	--pkgname=mauikit-git \
-	--pkgversion=2.1.1+git+2 \
+	--pkgversion=2.1.2+git+1 \
 	--pkgarch=amd64 \
 	--pkgrelease="1" \
 	--pkglicense=LGPL-3 \
-	--pkggroup=lib \
+	--pkggroup=libs \
 	--pkgsource=mauikit \
 	--pakdir=../.. \
 	--maintainer=uri_herrera@nxos.org \
