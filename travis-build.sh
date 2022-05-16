086013f0fb275123ba6ef57e4ac8ec85d5f8886d
Uri Herrera <hello@uriherrera.com>
2022-05-16 16:08:02 -0500

    update pkg

--- travis-build.sh
+++ travis-build.sh
@@ -97,7 +97,7 @@ checkinstall -D -y \
 	--install=no \
 	--fstrans=yes \
 	--pkgname=mauikit-git \
-	--pkgversion=2.1.2+git+2 \
+	--pkgversion=2.1.2+git+3 \
 	--pkgarch=amd64 \
 	--pkgrelease="1" \
 	--pkglicense=LGPL-3 \
