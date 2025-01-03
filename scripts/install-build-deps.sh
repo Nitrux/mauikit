c46f992347698e24a0c75c8cc78b010d301374f8
Uri Herrera <hello@uriherrera.com>
2025-01-03 01:08:23 -0600

    fix deb pkg run deps

--- scripts/install-build-deps.sh
+++ scripts/install-build-deps.sh
@@ -6,33 +6,36 @@ else
     APT_COMMAND="apt"
 fi
 
-$APT_COMMAND update -q
-$APT_COMMAND install -y - --no-install-recommends \
-    appstream \
-    automake \
-    autotools-dev \
-    build-essential \
-    checkinstall \
-    cmake \
-    curl \
-    devscripts \
-    equivs \
-    extra-cmake-modules \
-    gettext \
-    git \
-    gnupg2 \
-    lintian \
-    nlohmann-json3-dev \
-    libxcb-icccm4-dev \
-    libxcb-shape0-dev \
-    qml-module-qtgraphicaleffects \
-    qml-module-qtquick-controls2 \
-    qml-module-qtquick-shapes \
-    qt6-base-dev \
-    qt6-declarative-dev \
-    qt6-svg-dev \
-    qt6-multimedia-dev \
-    libkf6notifications-dev \
-    libkf6coreaddons-dev \
-    libkf6i18n-dev \
+BUILD_DEPS='
+    appstream
+    automake
+    autotools-dev
+    build-essential
+    checkinstall
+    cmake
+    curl
+    devscripts
+    equivs
+    extra-cmake-modules
+    gettext
+    git
+    gnupg2
+    libkf6coreaddons-dev
+    libkf6i18n-dev
+    libkf6notifications-dev
     libkf6windowsystem-dev
+    libxcb-icccm4-dev
+    libxcb-shape0-dev
+    lintian
+    nlohmann-json3-dev
+    qml-module-qtgraphicaleffects
+    qml-module-qtquick-controls2
+    qml-module-qtquick-shapes
+    qt6-base-dev
+    qt6-declarative-dev
+    qt6-multimedia-dev
+    qt6-svg-dev
+'
+
+$APT_COMMAND update -q
+$APT_COMMAND install -y - --no-install-recommends $BUILD_DEPS
