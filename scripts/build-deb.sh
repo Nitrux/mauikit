9f59363e5a7bded9778659cdf1febbd112588e0c
UriHerrera <hello@uriherrera.com>
2025-06-12 21:55:48 -0600

    update ci scripts

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -1,33 +1,45 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -eu
+#############################################################################################################################################################################
+#   The license used for this file and its contents is: BSD-3-Clause                                                                                                        #
+#                                                                                                                                                                           #
+#   Copyright <2025> <Uri Herrera <uri_herrera@nxos.org>>                                                                                                                   #
+#                                                                                                                                                                           #
+#   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:                          #
+#                                                                                                                                                                           #
+#    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.                                        #
+#                                                                                                                                                                           #
+#    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer                                      #
+#       in the documentation and/or other materials provided with the distribution.                                                                                         #
+#                                                                                                                                                                           #
+#    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software                    #
+#       without specific prior written permission.                                                                                                                          #
+#                                                                                                                                                                           #
+#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,                      #
+#    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS                  #
+#    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE                 #
+#    GOODS OR SERVICES; LOSS OF USE, DATA,   OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,                      #
+#    STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   #
+#############################################################################################################################################################################
 
-### Update sources
 
-mkdir -p /etc/apt/keyrings
+# -- Exit on errors.
 
-curl -fsSL https://packagecloud.io/nitrux/mauikit/gpgkey | gpg --dearmor -o /etc/apt/keyrings/nitrux_mauikit-archive-keyring.gpg
+set -e
 
-cat <<EOF > /etc/apt/sources.list.d/nitrux-mauikit.list
-deb [signed-by=/etc/apt/keyrings/nitrux_mauikit-archive-keyring.gpg] https://packagecloud.io/nitrux/mauikit/debian/ trixie main
-EOF
 
-apt -q update
+# -- Download Source
 
-### Install Package Build Dependencies #2
-
-apt -q -y install --no-install-recommends \
-	maui-manager-git
-
-### Download Source
-
-git clone --depth 1 --branch $MAUIKIT_BRANCH https://invent.kde.org/maui/mauikit.git
+git clone --depth 1 --branch "$MAUIKIT_BRANCH" https://invent.kde.org/maui/mauikit.git
 
 rm -rf mauikit/{demo,LICENSE,README.md}
 
-### Compile Source
+
+# -- Compile Source
 
 mkdir -p build && cd build
+
+HOST_MULTIARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
 
 cmake \
 	-DCMAKE_INSTALL_PREFIX=/usr \
@@ -40,13 +52,15 @@ cmake \
 	-DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON \
 	-DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" \
 	-DCMAKE_VERBOSE_MAKEFILE=ON \
-	-DCMAKE_INSTALL_LIBDIR=/usr/lib/x86_64-linux-gnu ../mauikit/
+	-DCMAKE_INSTALL_LIBDIR="/usr/lib/${HOST_MULTIARCH}" \
+	../mauikit/
 
 make -j"$(nproc)"
 
 make install
 
-### Run checkinstall and Build Debian Package
+
+# -- Run checkinstall and Build Debian Package
 
 >> description-pak printf "%s\n" \
 	'A free and modular front-end framework for developing user experiences.' \
@@ -67,8 +81,8 @@ checkinstall -D -y \
 	--install=no \
 	--fstrans=yes \
 	--pkgname=mauikit-git \
-	--pkgversion=$PACKAGE_VERSION \
-	--pkgarch=amd64 \
+	--pkgversion="$PACKAGE_VERSION" \
+	--pkgarch="$(dpkg --print-architecture)" \
 	--pkgrelease="1" \
 	--pkglicense=LGPL-3 \
 	--pkggroup=libs \
--- scripts/install-build-deps.sh
+++ scripts/install-build-deps.sh
@@ -1,4 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+#############################################################################################################################################################################
+#   The license used for this file and its contents is: BSD-3-Clause                                                                                                        #
+#                                                                                                                                                                           #
+#   Copyright <2025> <Uri Herrera <uri_herrera@nxos.org>>                                                                                                                   #
+#                                                                                                                                                                           #
+#   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:                          #
+#                                                                                                                                                                           #
+#    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.                                        #
+#                                                                                                                                                                           #
+#    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer                                      #
+#       in the documentation and/or other materials provided with the distribution.                                                                                         #
+#                                                                                                                                                                           #
+#    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software                    #
+#       without specific prior written permission.                                                                                                                          #
+#                                                                                                                                                                           #
+#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,                      #
+#    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS                  #
+#    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE                 #
+#    GOODS OR SERVICES; LOSS OF USE, DATA,   OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,                      #
+#    STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   #
+#############################################################################################################################################################################
+
+
+# -- Exit on errors.
+
+set -e
+
+
+# -- Check if running as root.
 
 if [ "$EUID" -ne 0 ]; then
     APT_COMMAND="sudo apt"
@@ -6,36 +36,51 @@ else
     APT_COMMAND="apt"
 fi
 
-BUILD_DEPS='
-    appstream
-    automake
-    autotools-dev
-    build-essential
-    checkinstall
-    cmake
-    curl
-    devscripts
-    equivs
-    extra-cmake-modules
-    gettext
-    git
-    gnupg2
-    libkf6coreaddons-dev
-    libkf6i18n-dev
-    libkf6notifications-dev
-    libkf6windowsystem-dev
-    libxcb-icccm4-dev
-    libxcb-shape0-dev
-    lintian
-    nlohmann-json3-dev
-    qml-module-qtgraphicaleffects
-    qml-module-qtquick-controls2
-    qml-module-qtquick-shapes
-    qt6-base-dev
-    qt6-declarative-dev
-    qt6-multimedia-dev
-    qt6-svg-dev
-'
+
+# -- Install build packages.
 
 $APT_COMMAND update -q
-$APT_COMMAND install -y - --no-install-recommends $BUILD_DEPS
+$APT_COMMAND install -y - --no-install-recommends \
+    appstream \
+    automake \
+    autotools-dev \
+    build-essential \
+    checkinstall \
+    cmake \
+    curl \
+    devscripts \
+    equivs \
+    extra-cmake-modules \
+    gettext \
+    git \
+    gnupg2 \
+    libkf6coreaddons-dev \
+    libkf6i18n-dev \
+    libkf6notifications-dev \
+    libkf6windowsystem-dev \
+    libxcb-icccm4-dev \
+    libxcb-shape0-dev \
+    lintian \
+    nlohmann-json3-dev \
+    qml-module-qtgraphicaleffects \
+    qml-module-qtquick-controls2 \
+    qml-module-qtquick-shapes \
+    qt6-base-dev \
+    qt6-declarative-dev \
+    qt6-multimedia-dev \
+    qt6-svg-dev
+
+
+# -- Add package from our repository.
+
+mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://packagecloud.io/nitrux/mauikit/gpgkey | gpg --dearmor -o /etc/apt/keyrings/nitrux_mauikit-archive-keyring.gpg
+
+cat <<EOF > /etc/apt/sources.list.d/nitrux-mauikit.list
+deb [signed-by=/etc/apt/keyrings/nitrux_mauikit-archive-keyring.gpg] https://packagecloud.io/nitrux/mauikit/debian/ trixie main
+EOF
+
+$APT_COMMAND update -q
+$APT_COMMAND install -y - --no-install-recommends \
+	maui-manager-git
