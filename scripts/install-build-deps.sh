ba7f8529640ad03a7e850c2265047097df2ee6d8
Uri Herrera <hello@uriherrera.com>
2025-06-30 16:11:10 -0600

    test

--- scripts/install-build-deps.sh
+++ scripts/install-build-deps.sh
@@ -37,6 +37,35 @@ else
 fi
 
 
+
+# -- Add third-aprty repository keys.
+
+add_repo_key_and_source() {
+    local key_id="$1"
+    local keyring_file="$2"
+    local source_file="$3"
+    local source_content="$4"
+
+    if ! gpg --list-keys "$key_id" &>/dev/null; then
+        gpg --batch --keyserver hkps://keyserver.ubuntu.com --recv-keys "$key_id"
+    fi
+
+    if ! [ -f "$keyring_file" ] || ! gpg --quiet --with-colons --import-options show-only --import "$keyring_file" | grep -q "$key_id"; then
+        gpg --batch --yes --export "$key_id" | gpg --dearmor -o "$keyring_file"
+    fi
+
+    if ! grep -Fxq "$source_content" "$source_file" 2>/dev/null; then
+        echo "$source_content" > "$source_file"
+    fi
+}
+
+add_repo_key_and_source \
+    "E6D4736255751E5D" \
+    "/etc/apt/keyrings/kde_neon-archive-keyring.gpg" \
+    "/etc/apt/sources.list.d/neon-repo.list" \
+    "deb [signed-by=/etc/apt/keyrings/kde_neon-archive-keyring.gpg] https://origin.archive.neon.kde.org/stable/ jammy main"
+
+
 # -- Install build packages.
 
 $APT_COMMAND update -q
@@ -69,18 +98,3 @@ $APT_COMMAND install -y - --no-install-recommends \
     qt6-declarative-dev \
     qt6-multimedia-dev \
     qt6-svg-dev
-
-
-# -- Add package from our repository.
-
-mkdir -p /etc/apt/keyrings
-
-curl -fsSL https://packagecloud.io/nitrux/mauikit/gpgkey | gpg --dearmor -o /etc/apt/keyrings/nitrux_mauikit-archive-keyring.gpg
-
-cat <<EOF > /etc/apt/sources.list.d/nitrux-mauikit.list
-deb [signed-by=/etc/apt/keyrings/nitrux_mauikit-archive-keyring.gpg] https://packagecloud.io/nitrux/mauikit/debian/ forky main
-EOF
-
-$APT_COMMAND update -q
-$APT_COMMAND install -y - --no-install-recommends \
-	maui-manager
