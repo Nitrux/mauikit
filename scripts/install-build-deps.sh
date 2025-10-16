9545c26436321e83ef228e512708bf0d04d0ce46
Uri Herrera <hello@uriherrera.com>
2025-10-16 14:29:10 -0600

    create branch

--- scripts/install-build-deps.sh
+++ scripts/install-build-deps.sh
@@ -107,7 +107,7 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://packagecloud.io/nitrux/zbkit/gpgkey | gpg --dearmor -o /etc/apt/keyrings/nitrux_zbkit-archive-keyring.gpg
 
 cat <<EOF > /etc/apt/sources.list.d/nitrux-zbkit.list
-deb [signed-by=/etc/apt/keyrings/nitrux_zbkit-archive-keyring.gpg] https://packagecloud.io/nitrux/zbkit/debian/ bookworm main
+deb [signed-by=/etc/apt/keyrings/nitrux_zbkit-archive-keyring.gpg] https://packagecloud.io/nitrux/zbkit/debian/ bullseye main
 EOF
 
 $APT_COMMAND update -q
