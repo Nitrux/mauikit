54d7f947336963b2cb4fd9b35d36b3262cd539c0
Uri Herrera <hello@uriherrera.com>
2026-05-12 17:44:46 -0600

    use gh fork

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -11,7 +11,7 @@ set -e
 
 # -- Download Source
 
-git clone --depth 1 --branch "$MAUIKIT_BRANCH" https://invent.kde.org/maui/mauikit.git
+git clone --depth 1 --branch "$MAUIKIT_BRANCH" https://github.com/Nitrux/mauikit-src.git
 
 rm -rf mauikit/{demo,LICENSE,README.md}
 
