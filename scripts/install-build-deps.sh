e25b349aeff4d8e5b3992d66eb07f39e45ba78e9
Uri Herrera <hello@uriherrera.com>
2026-05-19 13:31:05 -0600

    fix typo in build script

--- scripts/install-build-deps.sh
+++ scripts/install-build-deps.sh
@@ -21,7 +21,7 @@ fi
 # -- Install build packages.
 
 $APT_COMMAND update -q
-$APT_COMMAND install -y - --no-install-recommends \
+$APT_COMMAND install -y --no-install-recommends \
     appstream \
     automake \
     autotools-dev \
@@ -69,5 +69,5 @@ Enabled: yes
 EOF
 
 $APT_COMMAND update -q
-$APT_COMMAND install -y - --no-install-recommends \
+$APT_COMMAND install -y --no-install-recommends \
     maui-manager
